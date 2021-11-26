26927efd19bad6ebdde1d97a27341a12c8134400
Peter <peter.rucek@gmail.com>
2021-11-26 12:33:25 +0100

    Delete unused code

--- public/views/user/delete.php
+++
@@ -1,2 +0,0 @@
-<!-- DELETE /users/{user} -->
-<!-- Straight up deletion of specified user - controller calls repository->delete(user) -->
--- public/views/user/store.php
+++
@@ -1,2 +0,0 @@
-<!-- POST /users -->
-<!-- create.php form goes to this URL with post METHOD ("USER HAS BEEN CREATED" View) -->
