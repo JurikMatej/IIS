c74b1273a5ad67eb1aff3a4c6ead77b85e902b5f
Peter <peter.rucek@gmail.com>
2021-11-26 12:18:18 +0100

    Admin cannot change others data

--- public/views/user/edit.php
+++ public/views/user/edit.php
@@ -6,13 +6,14 @@
 <div class="user-edit">
     <h3 style="margin-bottom: 30px;">Edit user</h3>
     <form action="update" method="post">
-    <label for="name"> Name:</label> <input type="text" name="name" value=<?=$user->getFirstName()?>  ><br>
-    <label for="surname"> Surname:</label> <input type="text" name="surname" value=<?=$user->getLastName()?>  ><br>
-    <label for="email"> E-mail:</label> <input type="text" name="email" value=<?=$user->getMail()?> ><br>
-    <label for="password"> Password:</label> <input type="password" name="password" value=<?=$user->getPassword()?> ><br>
-    <label for="address"> Address:</label> <input type="text" name="address" value=<?=$user->getAddress()?> ><br>
-    <label for="role"> Role:</label> 
-    <select name="role"> 
+    <label for="name"> Name:</label> <input type="text" name="name" id= "name" value=<?=$user->getFirstName()?>  required><br>
+    <label for="surname"> Surname:</label> <input type="text" name="surname" id= "surname" value=<?=$user->getLastName()?>  required><br>
+    <label for="email"> E-mail:</label> <input type="text" name="email" id="email" value=<?=$user->getMail()?> required><br>
+    <label for="password"> Password:</label> <input type="password" name="password" id="password" required value=<?=$_SESSION['password']?> >
+    <i class="bi bi-eye-slash" id="togglePassword"></i><br>
+    <label for="address"> Address:</label> <input type="text" name="address" id="address" value=<?=$user->getAddress()?> ><br>
+    <label for="role" id="label_role" hidden> Role:</label> 
+    <select name="role" id="role" hidden> 
         <?php foreach ($roles as $option): ?>
         <?php if ('Visitor'=== $option->role): ?>
             <?php  continue;?>
@@ -29,3 +30,31 @@
     <input type="submit" value="Edit user">
     </form>
 </div>
+
+<?php if ($_SESSION['role'] == "Admin" && $_SESSION['role'] != $user->getRole()) { ?>
+    <script>
+        document.getElementById('name').disabled = true;
+        document.getElementById('surname').disabled = true;
+        document.getElementById('email').disabled = true;
+        document.getElementById('password').disabled = true;
+        document.getElementById('address').disabled = true;
+        document.getElementById('togglePassword').hidden = "true";
+
+        document.getElementById('role').hidden = false;
+        document.getElementById('label_role').hidden = false;
+    </script>
+<?php } ?>
+
+<script>
+
+    const togglePassword = document.querySelector('#togglePassword');
+    const password = document.querySelector('#password');
+
+    togglePassword.addEventListener('click', function (e) {
+        // toggle the type attribute
+        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+        password.setAttribute('type', type);
+        // toggle the eye / eye slash icon
+        this.classList.toggle('bi-eye');
+    });
+</script>
