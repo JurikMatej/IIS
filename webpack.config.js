5be5d2906101d5ff68e4bcc8d4c06245312ab14b
JurikMatej <jurik.matejw@gmail.com>
2021-11-26 18:20:55 +0100

    Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development', // TODO rewrite to 'production'
+    // App entrypoint
+    entry: './public/assets/js/modules/main.js',
+    // Transpiled & bundled output (must be absolute path)
+    output: {
+        path: path.resolve(__dirname, 'public/assets/js'),
+        filename: 'bundle.js'
+    },
+    // Loaders (import system)
+    module: {
+        rules: [
+            // js loader
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                },
+            },
+            // css loader
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            },
+            // scss loader
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [
+            '.js',
+        ]
+    }
+}
